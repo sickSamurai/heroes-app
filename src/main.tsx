790fce50f960bfc58241bd384d01c5fdb3183ddf
Laurent Chaverra <42363109+sickSamurai@users.noreply.github.com>
2022-10-10 18:01:38 -0500

    primer commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MainApp from "./MainApp";
+
+const root = document.getElementById("root")!;
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <MainApp />
+  </React.StrictMode>
+)
